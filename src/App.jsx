f28cb24864370b7f08202018ffe4b64192a3964d
Aman Kumar <amanthecoder001@gmail.com>
2026-09-11 20:09:16 +0530

    added heatmap

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ function App() {
                     <DashboardHeader />
                     <QuickStats />
                     <PlatformCards />
+                    <CodingHeatmap/>
                     <RatingProgress />
                 </>
             )}
